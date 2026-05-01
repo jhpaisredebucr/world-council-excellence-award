bb50df34fefd55fd85730e6786d66ec8c51aca50
keisackyu <jhoremzcruz@gmail.com>
2026-05-01 19:53:03 +0800

    del: random notif page

--- src/app/notifications/page.js
+++
@@ -1,9 +0,0 @@
-import NotificationsList from "@/app/components/notifications/NotificationsList";
-
-export default function NotificationsPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <NotificationsList />
-    </div>
-  );
-}
